ba864d87c06e97c91123f1e2d7d4207e99c0ff9f
shamsiddin01 <shamsiddintoshpolatov13@gmail.com>
2022-07-21 18:06:47 +0500

    banner model friest

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import ContactModel, BannerModel
 
-# Register your models here.
+
+@admin.register(ContactModel)
+class ContactModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'created_at']
+    list_display_links = ['name', 'email']
+    search_fields = ['name', 'email', 'massage']
+    list_filter = ['created_at']
+
+
+@admin.register(BannerModel)
+class BannerModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'collection', 'is_active']
+    list_display_links = ['title', 'collection']
+    search_fields = ['title', 'collection']
+    list_filter = ['created_at']
+
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class ContactModel(models.Model):
+    name = models.CharField(max_length=35, verbose_name=_('name'))
+    email = models.EmailField(verbose_name=_('email'))
+    massage = models.TextField(verbose_name=_('massage'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created_at'))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'contact'
+        verbose_name_plural = 'contacts'
+
+
+class BannerModel(models.Model):
+    collection = models.CharField(max_length=60, verbose_name=_('collection'))
+    title = models.CharField(max_length=65, verbose_name=_('title'))
+    description = models.CharField(max_length=100, verbose_name=_('description'))
+    image = models.ImageField(upload_to='banners/', verbose_name=_('image'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created_at'))
+    is_active = models.BooleanField(default=False, blank=True, verbose_name=_('is active'))
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'banner'
+        verbose_name_plural = 'banners'
