be8835f59d48113a9b44f3f9f6a2a262a67d98a9
shamsiddin01 <shamsiddintoshpolatov13@gmail.com>
2022-07-30 17:28:19 +0500

    added sale filter

--- shop/signals.py
+++ shop/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from .models import ProductModel
 
@@ -10,3 +10,13 @@ def get_real_price(sender, instance, *args, **kwargs):
         instance.real_price = ((100 - instance.discount) / 100) * instance.price
     else:
         instance.real_price = instance.price
+
+
+@receiver(pre_save, sender=ProductModel)
+def set_sale(sender, instance, *args, **kwargs):
+
+    if instance.is_discount():
+        instance.sale = True
+
+    else:
+        instance.sale = False
